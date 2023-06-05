12758299f48cabda39e6e21da59e94653ef4ef6f
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-06-04 19:20:44 -0500

    added youtube player api for enhanced control over embedded videos on the site

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
 const resultsContainer = document.getElementById("results");
 const modalsContainer = document.getElementById("modals");
+
 document.getElementById("startBtn").addEventListener("click", function (event) {
   fetchMovieResults();
   saveStremaingPlatform();
 });
+
 function saveStremaingPlatform() {
   var selectedStreaming = [];
   var checkedBoxes = $('input[type="checkbox"]:checked');
@@ -13,15 +15,18 @@ function saveStremaingPlatform() {
   }
   localStorage.setItem("platform", JSON.stringify(selectedStreaming));
 }
+
 function fetchMovieResults() {
   var selectedGenre = $("#genreSelect").val();
   var selectedStreaming = [];
   var keyword = $("#keywordInput").val();
   var checkedBoxes = $('input[type="checkbox"]:checked');
+
   for (var i = 0; i < checkedBoxes.length; i++) {
     var value = $(checkedBoxes[i]).val();
     selectedStreaming.push(value);
   }
+
   var streamingParam = selectedStreaming.join("%2C");
   var apiURL = `https://streaming-availability.p.rapidapi.com/v2/search/basic?country=us&services=${streamingParam}&output_language=en&show_type=movie&genre=${selectedGenre}&show_original_language=en&keyword=${keyword}`;
   const settings = {
@@ -34,21 +39,25 @@ function fetchMovieResults() {
       "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
     },
   };
+
   $.ajax(settings).done(function (response) {
     console.log(response);
     for (let i = 0; i < 8; i++) {
       const title = response.result[i].title;
       const releaseYear = response.result[i].year;
       const movieImg = response.result[i].backdropURLs.original;
+      const poster = response.result[i].posterURLs.original;
       const overview = response.result[i].overview;
-      const runtime = response.result[i].runtime; 
+      const runtime = response.result[i].runtime;
       const rating = response.result[i].imdbRating;
-      const YoutubeLink = response.result[i].youtubeTrailerVideoLink; 
+      const youtubeLink = response.result[i].youtubeTrailerVideoLink;
+      const youtubeId = response.result[i].youtubeTrailerVideoId;
+
       // Create a movie item element
       const movieItem = document.createElement("div");
       movieItem.classList.add("results-item");
+
       // Populate the movie item with data
-      // <button class="button open-button">More Info</button>
       movieItem.innerHTML =
         `<img class="movieImgThumbNail" src="${movieImg}">
           <h3>${title} - ${releaseYear}</h3>
@@ -59,11 +68,12 @@ function fetchMovieResults() {
         `<div id="modal${i}" class="modal">
         <div class="modal-content">
           <h4>${title}</h4>
-          <p>${releaseYear}</p>
+          <img src="${poster}" class="poster">
+          <p>${releaseYear} | Runtime: ${runtime} minutes | IMDb Score ${rating}/100</p>
           <p>${overview}</p>
-          <p>Runtime Minutes: ${runtime}</p>
-          <p>IMDb Rating Score: ${rating}</p>
-          <a href="${YoutubeLink}">Watch the Trailer on Youtube!</a>
+          <a href="${youtubeLink}">Watch the Trailer on Youtube!</a>
+          <br>
+          <div class="youtube-player" data-id="${youtubeId}"></div>
         </div>
         <div class="modal-footer">
           <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
@@ -74,10 +84,30 @@ function fetchMovieResults() {
       resultsContainer.appendChild(movieItem);
       modalsContainer.appendChild(modalItem);
     }
-    // $(document).ready(function(){
-      $('.modal').modal();
-    // });
+    $('.modal').modal();
+    loadYouTubePlayers();
   });
-
 }
 
+function loadYouTubePlayers() {
+  const youtubePlayers = document.querySelectorAll('.youtube-player');
+
+  youtubePlayers.forEach(function (playerElement) {
+    const videoId = playerElement.getAttribute('data-id');
+    const player = new YT.Player(playerElement, {
+      videoId: videoId,
+      events: {
+        'onReady': onPlayerReady
+      }
+    });
+  });
+}
+
+function onPlayerReady(event) {
+  const closeButton = event.target.getIframe().closest('.modal').querySelector('.modal-close');
+
+  closeButton.addEventListener('click', function () {
+    event.target.stopVideo();
+    event.target.getPlayerState = -1;
+  });
+}
